c46429a740e46656add679d2033498467d0b0e50
pradeep <pradeep.bhamu96@gmail.com>
2023-04-15 17:58:31 +0530

    first commit

---
+++ src/main/java/ecommerce/NetCart/NetCartApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.NetCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecommerce/NetCart/NetCartApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.NetCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
